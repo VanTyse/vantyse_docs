d66096e36facf7a7ae35e08ea010679361425bfc
Kingsley Okafor <nonsokingsley61@gmail.com>
2023-10-05 20:37:17 +0100

    style fix

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,26 +1,35 @@
-import React, {useContext} from 'react'
-import {Link} from 'react-router-dom'
-import './Header.css'
-import Button from '../Button/button'
-import { UserContext } from '../../context/context';
+import React, { useContext } from "react";
+import { Link } from "react-router-dom";
+import "./Header.css";
+import Button from "../Button/button";
+import { UserContext } from "../../context/context";
 
 function Header() {
-  const {user} = useContext(UserContext)
+  const { user } = useContext(UserContext);
   return (
     <header>
-        <Link to='/' style={{textDecoration: 'none', color: 'inherit'}}>
+      <Link to="/" style={{ textDecoration: "none", color: "inherit" }}>
         <div className="header-brand">
-            <div className="icon">
-                <i class="bi bi-file-earmark"></i>
-            </div>
-            <h1 className="brand-name"> <em>VANTYSE DOCS</em> </h1>
+          <div className="icon">
+            <i class="bi bi-file-earmark"></i>
+          </div>
+          <h1 className="brand-name">
+            {" "}
+            <em>VANTYSE DOCS</em>{" "}
+          </h1>
         </div>
+      </Link>
+      <div className="login-btn">
+        <Link
+          className="login-btn-appear-mobile"
+          style={{ textDecoration: "none" }}
+          to="/auth"
+        >
+          <Button type="roundedButton" value={user ? "Logout" : "Login"} />
         </Link>
-        <div className="login-btn">
-        <Link className="login-btn-appear-mobile" style={{textDecoration : 'none'}} to='/auth'><Button type='roundedButton' value={user ? "Logout"  : "Login"}/></Link>
-        </div>
+      </div>
     </header>
-  )
+  );
 }
 
-export default Header
+export default Header;
